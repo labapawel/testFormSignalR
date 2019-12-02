8ffdab0721ec66d4a615a11fa7c419dc534f677d
PL <labapawel@gmail.com>
2019-12-02 21:02:26 +0100

    init.

---
+++ TestFormSignalR/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TestFormSignalR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ TestFormSignalR/Form1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using System.Windows.Forms;
+
+namespace TestFormSignalR
+{
+    public partial class Form1 : Form
+    {
+        private IDisposable SignalR { get; set; }
+        const string ServerURI = "http://localhost:8080";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        internal void WriteToConsole(String message)
+        {
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                SignalR = WebApp.Start(ServerURI);
+            }
+            catch (TargetInvocationException)
+            {
+                WriteToConsole("Server failed to start. A server is already running on " + ServerURI);
+                //Re-enable button to let user try to start server again 
+           //     this.Invoke((Action)(() => ButtonStart.Enabled = true));
+                return;
+            }
+         //   this.Invoke((Action)(() => ButtonStop.Enabled = true));
+            WriteToConsole("Server started at " + ServerURI);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(ServerURI);
+            Task.Run(() => StartServer());
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (SignalR != null)
+            {
+                SignalR.Dispose();
+            }
+        }
+    }
+
+    /// <summary> 
+    /// Used by OWIN's startup process.  
+    /// </summary> 
+    class Startup
+    {
+
+        private FileServerOptions ConfigureFileSystem(IAppBuilder appBuilder)
+        {
+            PhysicalFileSystem physicalFileSystem = new PhysicalFileSystem(@".\wwwroot");
+            FileServerOptions fileOptions = new FileServerOptions();
+
+            fileOptions.EnableDefaultFiles = true;
+            fileOptions.RequestPath = PathString.Empty;
+            fileOptions.FileSystem = physicalFileSystem;
+            fileOptions.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+            fileOptions.StaticFileOptions.FileSystem = fileOptions.FileSystem = physicalFileSystem;
+            fileOptions.StaticFileOptions.ServeUnknownFileTypes = true;
+
+            return fileOptions;
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            FileServerOptions fileServerOptions = this.ConfigureFileSystem(app);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseFileServer(fileServerOptions);
+            app.MapSignalR();
+        }
+    }
+    /// <summary> 
+    /// Echoes messages sent using the Send message by calling the 
+    /// addMessage method on the client. Also reports to the console 
+    /// when clients connect and disconnect. 
+    /// </summary> 
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+        public override Task OnConnected()
+        {
+      //      Program.MainForm.WriteToConsole("Client connected: " + Context.ConnectionId);
+            return base.OnConnected();
+        }
+    /*    public override Task OnDisconnected()
+        {
+            Program.MainForm.WriteToConsole("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnected();
+        }*/
+    }
+
+}
